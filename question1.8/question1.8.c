f074980f0439235c45ce8074d2e16fb7b2dddd33
Jieyou Xu (Joe) <jieyouxu@outlook.com>
2018-10-04 15:20:09 +0100

    added exercises 1.8 and 1.9

---
+++ question1.8/question1.8.c
@@ -0,0 +1,11 @@
+#include "../graphics/graphics.h"
+
+int main(void) {
+    drawOval(50, 50, 100, 50);
+    drawOval(50, 50, 120, 70);
+    drawOval(50, 50, 140, 90);
+    drawOval(50, 50, 160, 110);
+    drawOval(50, 50, 180, 130);
+    drawOval(50, 50, 200, 150);
+    drawOval(50, 50, 220, 170);
+}
---
+++ question1.9/question1.9.c
@@ -0,0 +1,9 @@
+#include "../graphics/graphics.h"
+
+int main(void) {
+    drawString("Computer Science Department", 50, 50);
+    drawString("University College London", 50, 70);
+    drawString("Gower Street", 50, 90);
+    drawString("London", 50, 110);
+    drawString("WC1E 6BT", 50, 130);
+}
