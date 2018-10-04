2c0fb294c1c5534b73c79d87ff6f18ababaf439b
Jieyou Xu (Joe) <jieyouxu@outlook.com>
2018-10-04 10:18:13 +0100

    added exercise 1.7

---
+++ question1.7/question1.7.c
@@ -0,0 +1,8 @@
+#include "../graphics/graphics.h"
+
+int main(void) {
+    drawRect(50, 100, 80, 150);
+    drawRect(130, 130, 100, 60);
+    drawRect(150, 20, 80, 90);
+    drawRect(150, 200, 80, 50);
+}
